5536bdcd9e26c86a5d97e51e6b8e539514a0b1da
Dom Webber <dom.webber@hotmail.com>
2024-05-08 08:34:48 +0100

    apply prettier formatting

--- .changeset/polite-flies-repeat.md
+++ .changeset/polite-flies-repeat.md
@@ -2,4 +2,5 @@
 "@relucent.dev/prettier-config": minor
 ---
 
-use @ianvs/prettier-plugin-sort-imports over @trivago/prettier-plugin-sort-imports
+use @ianvs/prettier-plugin-sort-imports over
+@trivago/prettier-plugin-sort-imports
--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,15 @@
  * @see       https://relucent.dev
  */
 
-import sortImports from "@ianvs/prettier-plugin-sort-imports";
-import shellScript from "prettier-plugin-sh";
 import packageFile from "prettier-plugin-pkg";
+import shellScript from "prettier-plugin-sh";
+import sortImports from "@ianvs/prettier-plugin-sort-imports";
 import prettierXML from "@prettier/plugin-xml";
 
 /** @type {import("prettier").Config & import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const config = {
   proseWrap: "always",
-  plugins: [
-    sortImports,
-    shellScript,
-    packageFile,
-    prettierXML,
-  ],
+  plugins: [sortImports, shellScript, packageFile, prettierXML],
   importOrder: [
     // Preceding Space
     // See: https://github.com/IanVS/prettier-plugin-sort-imports?tab=readme-ov-file#6-enforce-a-blank-line-after-top-of-file-comments
